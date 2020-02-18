118988e6ed391b5af787593fa34cfeda4c469524
seunghyo <seunghyo@itshan.co.kr>
2020-02-18 16:58:19 +0900

    makeUI

---
+++ kawaitodo/App.js
@@ -0,0 +1,70 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar, TextInput, Dimensions, Platform
+} from 'react-native';
+
+const { height, width } = Dimensions.get("window");
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+       <StatusBar barStyle="light-content" />
+       <Text style={styles.title}>Kawai To do</Text>
+       <View style={styles.card}>
+          <TextInput style={styles.input} placeholder={"New To Do"} />
+       </View>
+      </View>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+    backgroundColor:'#f23657',
+    alignItems: 'center',
+    
+  },
+  title : {
+    color: 'white',
+    fontSize: 30,
+    marginTop: 50,
+    fontWeight: "200",
+    marginBottom: 30,
+  },
+  card:{
+    backgroundColor: "white",
+    flex:1,
+    width: width - 25,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    ...Platform.select({
+      ios: {
+        shadowColor: 'rgb(50, 50, 50)',
+        shadowOpacity : 0.5,
+        shadowRadius : 5,
+        shadowOffset:{
+          height: -1,
+          width: 0
+        }
+      },
+      android: {
+        elevation: 3
+      }
+    })
+  }
+});
+
